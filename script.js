6281a5a14a326c9b2881182b7d44f3097ab962a0
Tatzzelwurm <131162981+Tatzzelwurm@users.noreply.github.com>
2025-12-27 00:59:26 +0700

    feat: Create a 16x16 grid with div elements

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector("#container")
+let array = []
+
+for (let i = 1; i <= 256; i++) {
+     const element = document.createElement("div")
+     array.push(element)
+}
+
+array.forEach(arr => {
+     container.appendChild(arr)
+     arr.classList.add("divGrid")
+
+})
+
