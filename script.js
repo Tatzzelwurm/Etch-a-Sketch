b6d4f54891d72446b28d04b3fc6169de7261624a
Tatzzelwurm <131162981+Tatzzelwurm@users.noreply.github.com>
2025-12-27 17:54:19 +0700

    feat: Add grid resize button with user input prompt

--- script.js
+++ script.js
@@ -9,10 +9,26 @@ for (let i = 1; i <= 256; i++) {
 array.forEach(arr => {
      container.appendChild(arr)
      arr.classList.add("divGrid")
-
 })
 
 array.forEach(arr => {
      arr.addEventListener("mouseenter",() => arr.style.backgroundColor = "lightblue")
 })
+
+const gridSizeBtn = document.querySelector("#gridSizeBtn")
+
+
+
+
+function getNewGridSize() {
+     const newGridSize = parseInt(prompt("Grid size (e.g., 16 for 16x16):"))
+     if (newGridSize <= 100 && newGridSize > 5) {
+          return newGridSize
+     }
+     else {
+          return "ERROR"
+     }
+}
+
+
 
