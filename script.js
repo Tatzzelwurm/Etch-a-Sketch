8062d47724d7a8bcc8ca86dfb882a091a8dbb3ad
Tatzzelwurm <131162981+Tatzzelwurm@users.noreply.github.com>
2025-12-28 15:20:56 +0700

    feat: add a function for user to change grid sizes

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
 const container = document.querySelector("#container")
+let gridSize = 16 // Grid dimensions (16x16)
+let gridElementCount = 256  // Total cells = gridSize * gridSize
 let array = []
 
-for (let i = 1; i <= 256; i++) {
-     const element = document.createElement("div")
-     array.push(element)
+createGrid(gridElementCount,gridSize)
+addNewGrid()
+draw()
+
+function draw() {
+array.forEach(arr => {
+     arr.addEventListener("mouseenter",() => arr.style.backgroundColor = "rgba(0, 0, 0, 0.788)")
+})
 }
 
+function getNewGridSize() {
+     const newGridSize = parseInt(prompt("Grid size (e.g., 16 for 16x16)\nmax 100, min 5:"))
+     if (newGridSize <= 100 && newGridSize >= 5) {
+          changeGridSize(newGridSize)
+     
+     }
+     else {
+          alert("Invalid input. Please enter a number from 5 to 100")
+     }
+}
+const gridSizeBtn = document.querySelector("#gridSizeBtn")
+gridSizeBtn.addEventListener("click", () => getNewGridSize())
+
+function changeGridSize(newGridSize, newGridElementCount) {
+     clearGrid()
+     newGridElementCount = newGridSize ** 2
+     createGrid(newGridElementCount, newGridSize)
+     addNewGrid()
+     draw()
+}
+
+
+function createGrid(newGridElementCount,newGridSize) {
+  for (let i = 1; i <= newGridElementCount; i++) {
+    const element = document.createElement("div");
+    element.style.width = `calc(100% / ${newGridSize})`;
+    element.style.height = `calc(100% / ${newGridSize})`;
+    array.push(element);
+  }
+
+}
+
+function addNewGrid() {
 array.forEach(arr => {
      container.appendChild(arr)
      arr.classList.add("divGrid")
 })
-
-array.forEach(arr => {
-     arr.addEventListener("mouseenter",() => arr.style.backgroundColor = "lightblue")
-})
-
-const gridSizeBtn = document.querySelector("#gridSizeBtn")
-
-
-
-
-function getNewGridSize() {
-     const newGridSize = parseInt(prompt("Grid size (e.g., 16 for 16x16):"))
-     if (newGridSize <= 100 && newGridSize > 5) {
-          return newGridSize
-     }
-     else {
-          return "ERROR"
-     }
 }
 
-
-
+function clearGrid() {
+array.forEach(arr => (
+     container.removeChild(arr)
+))
+array.length = 0
+}
